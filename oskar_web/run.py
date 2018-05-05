3bbbd2be4b9a1dacf82a93c83524412adf9481ab
Benjamin Mort <ben.mort@gmail.com>
2018-05-05 18:02:41 +0100

    updates

--- oskar_web/run.py
+++ oskar_web/run.py
@@ -14,7 +14,7 @@ def _get_releases():
     path = os.path.dirname(__file__)
     releases = []
     releases_dir = join(path, 'assets', 'releases')
-    for release_dir in os.listdir(releases_dir):
+    for release_dir in sorted(os.listdir(releases_dir), reverse=True):
         if release_dir in ['.DS_Store']:
             continue
         doc_dir = join(releases_dir, release_dir, 'docs')
