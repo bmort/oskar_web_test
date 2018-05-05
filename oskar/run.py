69cf29689036bcca96adeaa5f9cd6030e26f5223
Benjamin Mort <ben.mort@gmail.com>
2018-05-05 15:35:34 +0100

    initial commit

---
+++ oskar/run.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os
+from os.path import join
+from pprint import pprint
+from flask import Flask, render_template
+
+
+APP = Flask(__name__)
+
+
+def _get_releases():
+    """."""
+    print('here')
+    path = os.path.dirname(__file__)
+    releases = []
+    releases_dir = join(path, 'assets', 'releases')
+    for release_dir in os.listdir(releases_dir):
+        if release_dir in ['.DS_Store']:
+            continue
+        doc_dir = join(releases_dir, release_dir, 'docs')
+        docs = []
+        for _file in os.listdir(doc_dir):
+            if os.path.splitext(_file)[1] == '.pdf':
+                docs.append(_file)
+        releases.append(dict(version=release_dir, docs=docs))
+    pprint(releases)
+    return releases
+
+
+RELEASES = _get_releases()
+
+@APP.route('/')
+def home():
+    """."""
+    config=dict(new_release=True, releases=RELEASES)
+    return render_template('home.html', **config)
+
+@APP.route('/<version>')
+def release(version):
+    """."""
+    config=dict(releases=RELEASES)
+    return render_template('release.html', **config)
+
+@APP.route('/about')
+def about():
+    """."""
+    config=dict(releases=RELEASES)
+    return render_template('about.html', **config)
---
+++ render_static.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+
+Consider:
+<https://nicolas.perriault.net/code/2012/dead-easy-yet-powerful-static-website-generator-with-flask/>
+"""
+
+import os
+import shutil
+from distutils.dir_util import copy_tree
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+
+def main():
+    """."""
+    output_path = 'static_site'
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    env = Environment(
+        loader=PackageLoader('oskar', 'templates'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('home.html')
+    config=dict(new_release=True)
+    html = template.render(**config)
+
+    f = open(os.path.join(output_path, 'oskar.html'), 'w')
+    f.write(html)
+
+    for folder in ['static', 'assets']:
+        copy_tree(os.path.join('oskar', folder),
+                  os.path.join(output_path, folder))
+
+
+
+if __name__ == '__main__':
+    main()
