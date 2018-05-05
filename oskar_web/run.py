c654df6c6f70ab67331d8814e4ba6f253bd0a0a8
Benjamin Mort <ben.mort@gmail.com>
2018-05-06 00:56:20 +0100

    added a few more updates / experiments

--- oskar_web/run.py
+++ oskar_web/run.py
@@ -13,7 +13,7 @@ def _get_releases():
     print('here')
     path = os.path.dirname(__file__)
     releases = []
-    releases_dir = join(path, 'assets', 'releases')
+    releases_dir = join(path, 'static', 'releases')
     for release_dir in sorted(os.listdir(releases_dir), reverse=True):
         if release_dir in ['.DS_Store']:
             continue
@@ -33,21 +33,22 @@ RELEASES = _get_releases()
 @APP.route('/')
 def home():
     """."""
-    config=dict(new_release=True, releases=RELEASES)
+    config = dict(new_release=True, releases=RELEASES,
+                  downloads=[{'name': 'Telescope Models'}])
     return render_template('home.html', **config)
 
 
-@APP.route('/release/<version>')
+@APP.route('/releases/<version>')
 def release(version):
     """."""
-    config=dict(releases=RELEASES)
+    config = dict(releases=RELEASES, version=version)
     return render_template('release.html', **config)
 
 
 @APP.route('/faq')
 def faq():
     """."""
-    config=dict(releases=RELEASES)
+    config = dict(releases=RELEASES)
     return render_template('faq.html', **config)
 
 
@@ -56,3 +57,50 @@ def documentation():
     """."""
     config = dict(releaes=RELEASES)
     return render_template('documentation.html', **config)
+
+
+@APP.route('/quick_start')
+@APP.route('/start')
+def getting_start():
+    """."""
+    config = dict(releaes=RELEASES)
+    return render_template('getting_started.html', **config)
+
+
+@APP.route('/downloads')
+def downloads():
+    """."""
+    config = dict(releaes=RELEASES)
+    return render_template('downloads.html', **config)
+
+
+@APP.route('/releases')
+def releases():
+    """."""
+    config = dict(releaes=RELEASES)
+    return render_template('releases.html', **config)
+
+@APP.route('/data')
+def data():
+    """."""
+    config = dict(releaes=RELEASES)
+    return render_template('data.html', **config)
+
+
+@APP.route('/data/telescopes')
+def telescope_models():
+    """."""
+    config = dict(releaes=RELEASES)
+    return render_template('telescope_models.html', **config)
+
+@APP.route('/data/sky')
+def sky_models():
+    """."""
+    config = dict(releaes=RELEASES)
+    return render_template('sky_models.html', **config)
+
+@APP.route('/data/scripts')
+def scripts():
+    """."""
+    config = dict(releaes=RELEASES)
+    return render_template('scripts.html', **config)
