3fd6d99fbd434ab308e8b5f67953f7bc9b3a75de
Benjamin Mort <ben.mort@gmail.com>
2018-05-06 02:32:48 +0100

    updates

--- oskar_web/run.py
+++ oskar_web/run.py
@@ -3,104 +3,123 @@ import os
 from os.path import join
 from pprint import pprint
 from flask import Flask, render_template
+from datetime import datetime, date
 
 
 APP = Flask(__name__)
 
-
-def _get_releases():
-    """."""
-    print('here')
-    path = os.path.dirname(__file__)
-    releases = []
-    releases_dir = join(path, 'static', 'releases')
-    for release_dir in sorted(os.listdir(releases_dir), reverse=True):
-        if release_dir in ['.DS_Store']:
-            continue
-        doc_dir = join(releases_dir, release_dir, 'docs')
-        docs = []
-        for _file in os.listdir(doc_dir):
-            if os.path.splitext(_file)[1] == '.pdf':
-                docs.append(_file)
-        releases.append(dict(version=release_dir, docs=docs))
-    pprint(releases)
-    return releases
-
-
-RELEASES = _get_releases()
+DATA = {
+    "current_release": "2.5.1",
+    "release_path": "/static/releases",
+    "doc_ext": ".pdf",
+    "releases": {
+        "2.7.0":
+        {
+            "date": "05/05/18",
+        },
+        "2.6.1":
+        {
+            "date": "05/06/17",
+        },
+        "2.5.1":
+        {
+            "date": "05/06/16",
+            "docs": [
+                "Apps",
+                "Binary-File-Format",
+                "Example",
+                "Install",
+            ]
+        }
+    }
+}
 
 
 @APP.route('/')
 def home():
     """."""
-    config = dict(new_release=True, releases=RELEASES,
-                  downloads=[{'name': 'Telescope Models'}])
-    return render_template('home.html', **config)
+    now = datetime.now()
+    version = DATA['current_release']
+    _date = DATA['releases'][version]['date']
+    delta = now - datetime.strptime(DATA['releases'][version]['date'], '%d/%m/%y')
+    new_release = True if delta.days < 30 else False
+    return render_template('home.html',
+                           new_release=new_release,
+                           data=DATA)
 
-
-@APP.route('/releases/<version>')
-def release(version):
-    """."""
-    config = dict(releases=RELEASES, version=version)
-    return render_template('release.html', **config)
 
 
 @APP.route('/faq')
 def faq():
     """."""
-    config = dict(releases=RELEASES)
-    return render_template('faq.html', **config)
+    return render_template('faq.html',
+                           data=DATA)
 
 
 @APP.route('/documentation')
 def documentation():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('documentation.html', **config)
+    _path = os.path.join(DATA['release_path'], DATA['current_release'], 'docs')
+    _version = '.'.join(DATA['current_release'].split('.')[:2])
+    return render_template('documentation.html',
+                           doc_path=_path,
+                           doc_version=_version,
+                           data=DATA)
 
 
 @APP.route('/quick_start')
 @APP.route('/start')
 def getting_start():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('getting_started.html', **config)
+    return render_template('getting_started.html',
+                           data=DATA)
 
 
 @APP.route('/downloads')
 def downloads():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('downloads.html', **config)
+    return render_template('downloads.html',
+                           data=DATA)
 
 
 @APP.route('/releases')
 def releases():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('releases.html', **config)
+    return render_template('releases.html',
+                           data=DATA)
+
+
+@APP.route('/releases/<version>')
+def release(version):
+    """."""
+    return render_template('release.html',
+                           data=DATA,
+                           version=version)
+
 
 @APP.route('/data')
 def data():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('data.html', **config)
+    return render_template('data.html',
+                           data=DATA)
 
 
 @APP.route('/data/telescopes')
 def telescope_models():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('telescope_models.html', **config)
+    return render_template('telescope_models.html',
+                           data=DATA)
+
 
 @APP.route('/data/sky')
 def sky_models():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('sky_models.html', **config)
+    return render_template('sky_models.html',
+                           data=DATA)
+
 
 @APP.route('/data/scripts')
 def scripts():
     """."""
-    config = dict(releaes=RELEASES)
-    return render_template('scripts.html', **config)
+    return render_template('scripts.html',
+                           data=DATA)
